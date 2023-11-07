2d824093da067e377cbc17cab6d8fdad5d9ec65e
weareandrei <weareandrei@gmail.com>
2023-11-07 15:43:17 +0000

    Initial commit

---
+++ draw2d/draw.cpp
@@ -0,0 +1,90 @@
+#include "draw.hpp"
+
+#include <algorithm>
+
+#include <cmath>
+
+#include "surface.hpp"
+
+void draw_line_solid( Surface& aSurface, Vec2f aBegin, Vec2f aEnd, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments until the function
+	(void)aBegin;   // is properly implemented.
+	(void)aEnd;
+	(void)aColor;
+}
+
+void draw_triangle_wireframe( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments until the function
+	(void)aP0;   // is properly implemented.
+	(void)aP1;
+	(void)aP2;
+	(void)aColor;
+}
+
+void draw_triangle_solid( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments until the function
+	(void)aP0;   // is properly implemented.
+	(void)aP1;
+	(void)aP2;
+	(void)aColor;
+}
+
+void draw_triangle_interp( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorF aC0, ColorF aC1, ColorF aC2 )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: Remove the following when you start your implementation!
+	//TODO: This draws a solid triangle until you implement the function
+	//TODO: properly. (This is such that you can see the asteroids with solid
+	//TODO: shading until that point.)
+	draw_triangle_solid( aSurface, aP0, aP1, aP2, linear_to_srgb( aC0 ) );
+	(void)aC1;
+	(void)aC2;
+}
+
+
+void draw_rectangle_solid( Surface& aSurface, Vec2f aMinCorner, Vec2f aMaxCorner, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments until the function
+	(void)aMinCorner;   // is properly implemented.
+	(void)aMaxCorner;
+	(void)aColor;
+}
+
+void draw_rectangle_outline( Surface& aSurface, Vec2f aMinCorner, Vec2f aMaxCorner, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments
+	(void)aMinCorner;
+	(void)aMaxCorner;
+	(void)aColor;
+}
