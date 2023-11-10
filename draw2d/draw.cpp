960411402860246c8c9aff10988e97daf1133770
weareandrei <weareandrei@gmail.com>
2023-11-10 20:42:12 +0000

    1.4 completed. Main works as well as sandbox. Might need to review one more time.

--- draw2d/draw.cpp
+++ draw2d/draw.cpp
@@ -91,32 +91,40 @@ void draw_line_solid(Surface& aSurface, Vec2f aBegin, Vec2f aEnd, ColorU8_sRGB a
     }
 }
 
-void draw_triangle_wireframe( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )
-{
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-
-	//TODO: remove the following when you start your implementation
-	(void)aSurface; // Avoid warnings about unused arguments until the function
-	(void)aP0;   // is properly implemented.
-	(void)aP1;
-	(void)aP2;
-	(void)aColor;
+template<typename T>
+T clamp(T value, T min, T max) {
+    return std::max(min, std::min(value, max));
 }
 
-void draw_triangle_solid( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )
-{
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
+// Compute area of triangle using its vertices
+float triangle_area(const Vec2f& p0, const Vec2f& p1, const Vec2f& p2) {
+    return 0.5f * ((p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y));
+}
 
-	//TODO: remove the following when you start your implementation
-	(void)aSurface; // Avoid warnings about unused arguments until the function
-	(void)aP0;   // is properly implemented.
-	(void)aP1;
-	(void)aP2;
-	(void)aColor;
+void draw_triangle_solid(Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor) {
+    // Calculate bounding box of the triangle
+    float minX = std::floor(std::min({aP0.x, aP1.x, aP2.x}));
+    float maxX = std::ceil(std::max({aP0.x, aP1.x, aP2.x}));
+    float minY = std::floor(std::min({aP0.y, aP1.y, aP2.y}));
+    float maxY = std::ceil(std::max({aP0.y, aP1.y, aP2.y}));
+
+    // Iterate over the bounding box
+    for (float y = minY; y <= maxY; y++) {
+        for (float x = minX; x <= maxX; x++) {
+            // Calculate barycentric coordinates
+            float alpha = triangle_area(aP1, aP2, {x, y}) / triangle_area(aP1, aP2, aP0);
+            float beta = triangle_area(aP2, aP0, {x, y}) / triangle_area(aP2, aP0, aP1);
+            float gamma = triangle_area(aP0, aP1, {x, y}) / triangle_area(aP0, aP1, aP2);
+
+            // Check if the point is inside the triangle
+            if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+            	if (x >= 0 && x < aSurface.get_width() && y >= 0 && y < aSurface.get_height())
+            	{
+            		aSurface.set_pixel_srgb(static_cast<Surface::Index>(x), static_cast<Surface::Index>(y), aColor);
+            	}
+            }
+        }
+    }
 }
 
 void draw_triangle_interp( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorF aC0, ColorF aC1, ColorF aC2 )
