bf3451ac221cfff211e714014e0c80247586984e
weareandrei <weareandrei@gmail.com>
2023-11-10 20:57:12 +0000

    Colour is interpolated. Task is completed.

--- draw2d/draw.cpp
+++ draw2d/draw.cpp
@@ -129,20 +129,41 @@ void draw_triangle_solid(Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, Col
 
 void draw_triangle_interp( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorF aC0, ColorF aC1, ColorF aC2 )
 {
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
+	// Calculate bounding box of the triangle
+	float minX = std::floor(std::min({aP0.x, aP1.x, aP2.x}));
+	float maxX = std::ceil(std::max({aP0.x, aP1.x, aP2.x}));
+	float minY = std::floor(std::min({aP0.y, aP1.y, aP2.y}));
+	float maxY = std::ceil(std::max({aP0.y, aP1.y, aP2.y}));
 
-	//TODO: Remove the following when you start your implementation!
-	//TODO: This draws a solid triangle until you implement the function
-	//TODO: properly. (This is such that you can see the asteroids with solid
-	//TODO: shading until that point.)
-	draw_triangle_solid( aSurface, aP0, aP1, aP2, linear_to_srgb( aC0 ) );
-	(void)aC1;
-	(void)aC2;
+	// Iterate over the bounding box
+	for (float y = minY; y <= maxY; y++) {
+		for (float x = minX; x <= maxX; x++) {
+			// Calculate barycentric coordinates
+			float alpha = triangle_area(aP1, aP2, {x, y}) / triangle_area(aP1, aP2, aP0);
+			float beta = triangle_area(aP2, aP0, {x, y}) / triangle_area(aP2, aP0, aP1);
+			float gamma = triangle_area(aP0, aP1, {x, y}) / triangle_area(aP0, aP1, aP2);
+
+			// Check if the point is inside the triangle
+			if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+				// Interpolate color in linear RGB space
+				ColorF interpolatedColor = {
+					alpha * aC0.r + beta * aC1.r + gamma * aC2.r,
+					alpha * aC0.g + beta * aC1.g + gamma * aC2.g,
+					alpha * aC0.b + beta * aC1.b + gamma * aC2.b
+				};
+
+				// Convert linear RGB to sRGB before setting the pixel color
+				ColorU8_sRGB sRGBColor = linear_to_srgb(interpolatedColor);
+
+				// Set pixel color
+				if (x >= 0 && x < aSurface.get_width() && y >= 0 && y < aSurface.get_height()) {
+					aSurface.set_pixel_srgb(static_cast<Surface::Index>(x), static_cast<Surface::Index>(y), sRGBColor);
+				}
+			}
+		}
+	}
 }
-
-
+	
 void draw_rectangle_solid( Surface& aSurface, Vec2f aMinCorner, Vec2f aMaxCorner, ColorU8_sRGB aColor )
 {
 	//TODO: your implementation goes here
